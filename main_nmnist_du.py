730446ca83e744d56836f2fcf3ca224ba52fb08d
nvagus <togemig@hotmail.com>
2021-04-19 16:30:59 -0700

    add dual fc-column

---
+++ main_nmnist_du.py
@@ -0,0 +1,119 @@
+import click
+import torch
+from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
+
+from dataset import NMnistSampled
+from tnn import AutoMatchingMatrix, FullDualColumn
+
+
+class Interrupter:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        if exc_type is KeyboardInterrupt:
+            return True
+        return exc_type is None
+
+
+@click.command()
+@click.option('-g', '--gpu', default=0)
+@click.option('-e', '--epochs', default=1)
+@click.option('-b', '--batch', default=32)
+@click.option('-x', '--x-max', default=34)
+@click.option('-y', '--y-max', default=34)
+@click.option('-t', '--t-max', default=256)
+@click.option('-f', '--forced-dep', default=0)
+@click.option('-d', '--dense', default=0.15)
+@click.option('-w', '--w-init', default=0.5)
+@click.option('-s', '--step', default=16)
+@click.option('-l', '--leak', default=32)
+@click.option('-k', '--dual', default=0.05)
+@click.option('--capture', default=0.20)
+@click.option('--backoff', default=-0.20)
+@click.option('--search', default=0.01)
+@click.option('-S/-U', '--supervised/--unsupervised', default=True)
+@click.option('--train-path', default='data/n-mnist/TrainSP')
+@click.option('--test-path', default='data/n-mnist/TestSP')
+@click.option('--model-path', default='model/n-mnist-du')
+def main(
+    gpu, batch, epochs, supervised,
+    x_max, y_max, t_max,
+    step, leak,
+    forced_dep, dense, w_init, dual,
+    capture, backoff, search,
+    train_path, test_path, model_path,
+    **kwargs
+):
+    if torch.cuda.is_available():
+        dev = f'cuda:{gpu}'
+    else:
+        dev = 'cpu'
+    device = torch.device(dev)
+
+    print(
+        f'Device: {device}, Batch: {batch}, Epochs: {epochs}, Supervised: {supervised}')
+    print(f'Forced Dep: {forced_dep}, Dense: {dense}, Weight Init: {w_init}')
+
+    train_data_loader = DataLoader(NMnistSampled(
+        train_path, x_max, y_max, t_max, device=device), shuffle=True, batch_size=batch)
+    test_data_loader = DataLoader(NMnistSampled(
+        test_path, x_max, y_max, t_max, device=device), batch_size=batch)
+
+    model = FullDualColumn(
+        x_max * y_max, 1, input_channel=2, output_channel=10,
+        step=step, leak=leak,
+        dense=dense, fodep=forced_dep, w_init=w_init, dual=dual
+    ).to(device)
+
+    def descriptor():
+        return (
+            f"{','.join('{:.0f}'.format(x) for x in model.weight_pos.sum(axis=1))}; "
+            f"{','.join('{:.0f}'.format(x) for x in model.weight_neg.sum(axis=1))}"
+        )
+
+    for epoch in range(epochs):
+        model.train(mode=True)
+        print(f"epoch: {epoch}")
+        train_data_iterator = tqdm(train_data_loader)
+        train_data_iterator.set_description(descriptor())
+        with Interrupter():
+            for data, label in train_data_iterator:
+                input_spikes = data.reshape(-1, 2, x_max * y_max, t_max)
+                if supervised:
+                    output_spikes = model.forward(
+                        input_spikes, label.to(device), mu_capture=capture, mu_backoff=backoff, mu_search=search)
+                else:
+                    output_spikes = model.forward(input_spikes)
+                # output_spikes: bacth, channel, neuro, time
+                accurate = (output_spikes.sum((-3, -2, -1)) > 0).logical_and(
+                    output_spikes.sum((-2, -1)).argmax(-1) == label.to(device)).sum()
+                train_data_iterator.set_description(
+                    f'{descriptor()}; {output_spikes.sum()}, {accurate}')
+
+        model.train(mode=False)
+        auto_matcher = AutoMatchingMatrix(10, 10)
+        with Interrupter():
+            for data, label in tqdm(test_data_loader):
+                input_spikes = data.reshape(-1, 2, x_max * y_max, t_max)
+                output_spikes = model.forward(input_spikes)
+
+                has_spikes = output_spikes.sum((-3, -2, -1)) > 0
+                y_preds = output_spikes.sum((-2, -1)).argmax(-1)
+
+                for has_spike, y_pred, y_true in zip(has_spikes.cpu().numpy(), y_preds.cpu().numpy(), label.numpy()):
+                    if has_spike:
+                        auto_matcher.add_sample(y_true, y_pred)
+
+        with Interrupter():
+            print(auto_matcher.mat)
+            print(f'Coverage: {auto_matcher.mat.sum() / len(test_data_loader.dataset)}')
+            auto_matcher.describe_print_clear()
+            torch.save(model.state_dict(), model_path)
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
--- tnn/tnn.py
+++ tnn/tnn.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 from torch.distributions.exponential import Exponential
 
@@ -174,7 +173,7 @@ class ConvColumn(torch.nn.Module):
         print(
             f'Building convolutional TNN layer with theta={theta:.4f}, dense={dense:.4f}, fodep={fodep}')
 
-    def forward(self, input_spikes, mu_capture=0.2000, mu_backoff=-0.4000, mu_search=0.0001):
+    def forward(self, input_spikes, mu_capture=0.2000, mu_backoff=-0.2000, mu_search=0.0001):
         potentials = self.get_potentials(input_spikes)
         output_spikes = self.winner_takes_all(potentials)
         if self.training:
@@ -235,14 +234,16 @@ class ConvColumn(torch.nn.Module):
             self.weight.add_(update).clip_(0, 1)
 
 
-class RecurColumn(torch.nn.Module):
+class FullDualColumn(torch.nn.Module):
     def __init__(
         self,
         synapses, neurons, input_channel=1, output_channel=1,
         step=16, leak=32,
-        fodep=None, w_init=0., theta=None, dense=None
+        fodep=None, w_init=0., theta=None, dense=None,
+        dual=0.05
     ):
-        super(RecurColumn, self).__init__()
+        super(FullDualColumn, self).__init__()
+
         self.synapses = synapses
         self.neurons = neurons
         self.input_channel = input_channel
@@ -257,14 +258,126 @@ class RecurColumn(torch.nn.Module):
         self.response_function = StepFireLeak(step, leak)
         self.fodep = fodep = fodep or self.response_function.fodep
         assert fodep >= self.response_function.fodep, f'forced depression should be at least {self.response_function.fodep}'
-        # initialize weight to zeros first
-        self.weight = torch.zeros(self.output_channel * self.neurons,
-                                  (self.input_channel + self.output_channel) * self.synapses) + w_init
-        self.bias = torch.zeros(self.output_channel *
-                                self.neurons) + self.theta
+        self.dual = dual
+        assert dual < dense, 'too much duality, try to set a smaller one'
+        # initialize weight to w_init
+        self.weight_pos = torch.nn.parameter.Parameter(
+            Exponential(1 / w_init).sample((self.output_channel *
+                                            self.neurons, self.input_channel * self.synapses)).clip(0, 1),
+            requires_grad=True
+        )
+        self.weight_neg = torch.nn.parameter.Parameter(
+            torch.zeros(self.output_channel * self.neurons, self.input_channel * self.synapses),
+            requires_grad=True
+        )
         print(
-            f'Building recurrent TNN layer with theta={theta:.4f}, dense={dense:.4f}, fodep={fodep}')
+            f'Building full connected dual TNN layer with theta={theta:.4f}, dense={dense:.4f}, fodep={fodep}, dual={dual}')
 
-    def forward(self, input_spikes, labels=None):
+    def forward(self, input_spikes, labels=None, bias=0.5, mu_capture=0.2000, mu_backoff=-0.2000, mu_search=0.0001):
+        potentials = self.get_potentials(input_spikes, labels, bias)
+        output_spikes = self.winner_takes_all(potentials)
 
-        return
+        if self.training:
+            self.stdp(potentials, output_spikes,
+                      mu_capture=mu_capture, mu_backoff=mu_backoff, mu_search=mu_search)
+
+        return output_spikes
+
+    def get_potentials(self, input_spikes, labels=None, bias=0.5):
+        assert self.dual < (1 - bias) * self.dense, 'This bias is too large to use with the current dual value'
+        # coalesce input channel and synpases
+        batch, channel, synapses, time = input_spikes.shape
+        input_spikes = input_spikes.reshape(batch, channel * synapses, time)
+        # perform conv1d to compute potentials
+        w_pos_kernel = self.response_function.get_weight_kernel(self.weight_pos)
+        w_neg_kernel = self.response_function.get_weight_kernel(self.weight_neg)
+        potentials = torch.nn.functional.conv1d(
+            input_spikes, w_pos_kernel - self.dual * w_neg_kernel, padding=self.response_function.padding)
+        with torch.no_grad():
+            dual_bias = self.dual * self.weight_neg.mean(-1, keepdim=True).unsqueeze(0)
+        potentials += dual_bias
+
+        # expand output channel and neurons
+        potentials = potentials.reshape(
+            batch, self.output_channel, self.neurons, -1)
+        if labels is not None:
+            batch, channel, neurons, time = potentials.shape
+            supervision = torch.zeros(batch, channel, dtype=torch.int32, device=labels.device).scatter(
+                1, labels.unsqueeze(-1), bias * self.theta
+            ).unsqueeze(-1).expand(-1, -1, neurons).unsqueeze(-1)
+            # supervision (batch, channel, neurons, 1)
+            potentials = potentials + supervision
+        else:
+            potentials = potentials + bias * self.theta
+        return potentials
+
+    def winner_takes_all(self, potentials):
+        batch, channel, neurons, time = potentials.shape
+
+        # move time axis to 0 for better performance
+        potentials = potentials.permute(3, 0, 2, 1)
+
+        # time to step out of depression, with initial 0 and constrains >= 0
+        depression = torch.zeros(
+            batch, neurons, channel, dtype=torch.int32, device=potentials.device)
+        # return winners of the same shape as potentials
+        winners = torch.zeros(time, batch, neurons, channel,
+                              dtype=torch.int32, device=potentials.device)
+
+        # iterate time axis: get the winner for each batch, channel of neurons, update winners and depression
+        for t in range(time):
+            # channel, batch, neurons
+            potential_t = potentials[t] * (depression == 0).int()
+            winner_t = potential_t.argmax(-1).unsqueeze(-1)
+
+            spike_t = (potential_t.gather(-1, winner_t)
+                       > self.theta).int()
+            winners[t].scatter_(-1, winner_t, spike_t)
+            depression += winners[t].sum(-1).unsqueeze(-1) * self.fodep
+            depression = (depression - 1).clip(0, self.fodep - 1)
+            # TODO k limitation per channel
+
+        return winners.permute(1, 3, 2, 0).float()
+
+    def stdp(
+        self,
+        potentials, output_spikes,
+        mu_capture, mu_backoff, mu_search
+    ):
+        batch, _channel, neurons, _time = output_spikes.shape
+
+        active_potentials = (potentials * output_spikes).sum()
+        total_spikes = output_spikes.sum((0, 2, 3)).unsqueeze(-1)
+
+        capture_grad, = torch.autograd.grad(active_potentials, self.weight_pos, retain_graph=True)
+        surrander_grad, = torch.autograd.grad(active_potentials, self.weight_neg, retain_graph=True)
+        surrander_grad /= self.dual
+        search_grad, = torch.autograd.grad(potentials.sum(), self.weight_pos) 
+        search_grad /= (self.weight_pos * self.response_function.kernel_size).floor() + 1
+
+        backoff_grad = total_spikes - capture_grad
+        compete_grad = -total_spikes - surrander_grad
+
+        update_pos = (
+            capture_grad * mu_capture +
+            backoff_grad * mu_backoff +
+            search_grad * mu_search
+        ) * (
+            (self.weight_pos * (1 - self.weight_pos) * 3 + 0.25)
+        ) / (
+            batch * neurons
+        )
+
+        update_neg = (
+            surrander_grad * mu_backoff + 
+            compete_grad * mu_capture
+        ) * (
+            (self.weight_neg * (1 - self.weight_pos) * 3 + 0.25)
+        ) / (
+            batch * neurons
+        )
+
+        with torch.no_grad():
+            self.weight_pos.add_(update_pos).clip_(0, 1)
+            self.weight_neg.add_(-update_neg).clip_(0, 1)
+
