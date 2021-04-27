d208cc5b1804949c1029fd1a93442b6ba3615818
tim <tanzhenyennn@gmail.com>
2021-04-27 13:14:43 -0700

    add main du

--- main_nmnist_du.py
+++ main_nmnist_du.py
@@ -1,10 +1,12 @@
 import click
 import torch
+import numpy as np
 from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
 
 from dataset import NMnistSampled
 from tnn import AutoMatchingMatrix, FullDualColumn
+from sklearn.metrics import accuracy_score
 
 
 class Interrupter:
@@ -20,13 +22,16 @@ class Interrupter:
 @click.command()
 @click.option('-g', '--gpu', default=0)
 @click.option('-e', '--epochs', default=1)
-@click.option('-i', '--bias', default=0.5)
+@click.option('-i', '--bias', default=0.5)  # new
+@click.option('-k', '--decay', default=0.99)
 @click.option('-b', '--batch', default=32)
 @click.option('-x', '--x-max', default=34)
 @click.option('-y', '--y-max', default=34)
 @click.option('-t', '--t-max', default=256)
+@click.option('-r', '--winners', default=1)
+@click.option('-n', '--neurons', default=1)
 @click.option('-f', '--forced-dep', default=0)
-@click.option('-d', '--dense', default=0.1)
+@click.option('-d', '--dense', default=0.1)  # new
 @click.option('-s', '--step', default=16)
 @click.option('-l', '--leak', default=32)
 @click.option('-w', '--w-init', default=0.5)
@@ -39,10 +44,10 @@ class Interrupter:
 @click.option('--model-path', default='model/n-mnist-du')
 def main(
     gpu, batch, epochs, supervised,
-    x_max, y_max, t_max,
-    step, leak, bias,
-    forced_dep, dense, w_init,
-    capture, backoff, search,
+    x_max, y_max, t_max, neurons, winners,
+    step, leak,
+    forced_dep, dense, w_init, bias,
+    capture, backoff, search, decay,
     train_path, test_path, model_path,
     **kwargs
 ):
@@ -62,15 +67,17 @@ def main(
         test_path, x_max, y_max, t_max, device=device), batch_size=batch)
 
     model = FullDualColumn(
-        x_max * y_max, 1, input_channel=2, output_channel=10,
-        step=step, leak=leak,
+        x_max * y_max, neurons, input_channel=2, output_channel=10, winners=winners,
+        step=step, leak=leak, bias=bias,
         dense=dense, fodep=forced_dep, w_init=w_init
     ).to(device)
 
     def descriptor():
-        return (
-            f"{','.join(f'{x*100:.0f}' for x in model.weight.mean(axis=1))}; "
-        )
+        max_print = 10
+        s = f"{','.join(f'{x*100:.0f}' for x in model.weight.mean(axis=1)[:max_print])}; "
+        s += f"{','.join(f'{x*100:.0f}' for x in model.bias[:max_print])}; "
+
+        return s
 
     for epoch in range(epochs):
         model.train(mode=True)
@@ -82,7 +89,7 @@ def main(
                 input_spikes = data.reshape(-1, 2, x_max * y_max, t_max)
                 if supervised:
                     output_spikes = model.forward(
-                        input_spikes, label.to(device), mu_capture=capture, mu_backoff=backoff, mu_search=search)
+                        input_spikes, label.to(device), mu_capture=capture, mu_backoff=backoff, mu_search=search, beta_decay=decay)
                 else:
                     output_spikes = model.forward(input_spikes, bias=0.5)
                 # output_spikes: bacth, channel, neuro, time
@@ -93,6 +100,9 @@ def main(
 
         model.train(mode=False)
         auto_matcher = AutoMatchingMatrix(10, 10)
+
+        real_labels = []
+        predicted_labels = []
         with Interrupter():
             for data, label in tqdm(test_data_loader):
                 input_spikes = data.reshape(-1, 2, x_max * y_max, t_max)
@@ -105,12 +115,21 @@ def main(
                     if has_spike:
                         auto_matcher.add_sample(y_true, y_pred)
 
+                    predicted_labels.append(y_pred)
+                    real_labels.append(y_true)
+
         with Interrupter():
             print(auto_matcher.mat)
-            print(
-                f'Coverage: {auto_matcher.mat.sum() / len(test_data_loader.dataset)}')
-            auto_matcher.describe_print_clear()
+            if np.all(np.sum(auto_matcher.mat, axis=0)) and np.all(np.sum(auto_matcher.mat, axis=1)):
+                accuracy, precision, recall = auto_matcher.describe()
+                print(accuracy, precision, recall)
+            else:
+                accuracy, precision, recall = -1, -1, -1
+
+            accuracy = accuracy_score(real_labels, predicted_labels)
             torch.save(model.state_dict(), model_path)
+
+            print(accuracy, precision, recall)
 
     return 0
 
