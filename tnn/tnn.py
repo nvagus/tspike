cecb2199613e1b281cd1188187444cffbf6f6365
Mingxiao An <togemig@gmail.com>
2021-05-01 23:27:57 -0700

    draft cnn with bias

--- tnn/tnn.py
+++ tnn/tnn.py
@@ -1,6 +1,7 @@
 import torch
 from torch.distributions.exponential import Exponential
 import torch.nn as nn
+import numpy as np
 
 
 class StepFireLeakKernel(torch.autograd.Function):
@@ -163,40 +164,52 @@ class ConvColumn(nn.Module):
         self,
         input_channel=1, output_channel=1,
         kernel=3, stride=2,
-        step=16, leak=32,
+        step=16, leak=32, bias=0.5, winners=0.5,
         fodep=None, w_init=None, theta=None, dense=None
     ):
         super(ConvColumn, self).__init__()
+        # model skeleton parameters
         self.input_channel = input_channel
         self.output_channel = output_channel
         self.kernel = kernel
         self.stride = stride
 
+        # threshold parameters
         assert theta or dense, 'either theta or dense should be specified'
         self.theta = theta = theta or dense * (kernel * kernel * input_channel)
         self.dense = dense = dense or theta / (kernel * kernel * input_channel)
         assert dense < 2 * input_channel * kernel * \
             kernel, 'invalid theta or density, try setting a smaller value'
-        # default response function: StepFireLeak
+        w_init = w_init or dense
+
+        # spiking control parameters
         self.response_function = StepFireLeak(step, leak)
         self.fodep = fodep = fodep or self.response_function.fodep
         assert fodep >= self.response_function.fodep, f'forced depression should be at least {self.response_function.fodep}'
-        w_init = w_init or dense
-        # initialize weight to w_init
+        self.winners = winners
+
+        # initialize weight and bias
+        self.bias = nn.parameter.Parameter(torch.zeros(self.output_channel) + bias, requires_grad=False)
         self.weight = nn.parameter.Parameter(
             Exponential(1 / w_init).sample((self.output_channel,
                                             self.input_channel, self.kernel, self.kernel)).clip(0, 1),
             requires_grad=True
         )
         print(
-            f'Building convolutional TNN layer with theta={theta:.4f}, dense={dense:.4f}, fodep={fodep}')
+            'Building convolutional connected TNN layer with '
+            f'theta={theta:.4f}, '
+            f'dense={dense:.4f}, '
+            f'fodep={fodep}, ',
+            f'winners={winners}, '
+            f'bias={bias}'
+        )
 
-    def forward(self, input_spikes, mu_capture=0.2000, mu_backoff=-0.2000, mu_search=0.0001):
+    def forward(self, input_spikes, mu_capture=0.2000, mu_backoff=-0.2000, mu_search=0.0001, beta_decay=0.9):
         potentials = self.get_potentials(input_spikes)
         output_spikes = self.winner_takes_all(potentials)
         if self.training:
             self.stdp(potentials, output_spikes,
-                      mu_capture, mu_backoff, mu_search)
+                      mu_capture, mu_backoff, mu_search, beta_decay)
         return output_spikes
 
     def get_potentials(self, input_spikes):
@@ -206,9 +219,11 @@ class ConvColumn(nn.Module):
             self.weight).permute(0, 1, 4, 2, 3)
         potentials = nn.functional.conv3d(
             input_spikes, w_kernel,
+            bias=self.bias,
             stride=(1, self.stride, self.stride),
             padding=(self.response_function.padding, 0, 0)
         )
+
         return potentials.permute(0, 1, 3, 4, 2)
 
     def winner_takes_all(self, potentials):
@@ -217,23 +232,31 @@ class ConvColumn(nn.Module):
             batch, channel, neuron_x * neuron_y, time).permute(3, 0, 2, 1)
         # time to step out of depression, with initial 0 and constrains >= 0
         depression = torch.zeros(
-            batch, neuron_x * neuron_y, channel, dtype=torch.int32, device=potentials.device)
+            batch, neuron_x * neuron_y, dtype=torch.int32, device=potentials.device)
         # return winners of the same shape as potentials
         winners = torch.zeros(time, batch, neuron_x * neuron_y,
                               channel, dtype=torch.int32, device=potentials.device)
+        winner_fodep = np.ceil(self.winner * neuron_x * neuron_y)
         for t in range(time):
-            potential_t = potentials[t] * (depression == 0).int()
+            depress_t = (depression == 0).unsqueeze(-1).int()
+            k_depress_t = ((depression != 0).sum(-1) < winner_fodep).int().reshape(-1, 1, 1)
+            potential_t = potentials[t] * depress_t * k_depress_t
             winner_t = potential_t.argmax(-1).unsqueeze(-1)
             spike_t = (potential_t.gather(-1, winner_t) > self.theta).int()
             winners[t].scatter_(-1, winner_t, spike_t)
-            depression += winners[t].sum(-1).unsqueeze(-1) * self.fodep
+            depression += winners[t].sum(-1) * self.fodep
             depression = (depression - 1).clip(0, self.fodep - 1)
-            # TODO k limitation per channel
+        
         return winners.permute(1, 3, 2, 0).reshape(batch, channel, neuron_x, neuron_y, time).float()
 
-    def stdp(self, potentials, output_spikes, mu_capture, mu_backoff, mu_search):
+    def stdp(
+        self, 
+        potentials, output_spikes, 
+        mu_capture, mu_backoff, mu_search, beta_decay
+    ):
         batch, _channel, neuron_x, neuron_y, _time = output_spikes.shape
         total_spike = output_spikes.sum((0, 2, 3, 4)).reshape(-1, 1, 1, 1)
+        has_spikes = (total_spike > 0).int()
 
         capture_grad, = torch.autograd.grad(
             (potentials * output_spikes).sum(), self.weight, retain_graph=True)
@@ -241,19 +264,21 @@ class ConvColumn(nn.Module):
         backoff_grad = total_spike - capture_grad
         search_grad, = torch.autograd.grad(
             potentials.sum() / self.response_function.kernel_size, self.weight)
+        search_grad = search_grad * (capture_grad == 0).int()
 
-        update = (
-            capture_grad * mu_capture +
+        weight_update = (
+            capture_grad * mu_capture * (1 - torch.tanh(self.weight)) +
             backoff_grad * mu_backoff +
-            search_grad * mu_search
-        ) * (
-            (self.weight * (1 - self.weight) * 3 + 0.25)
+            search_grad * mu_search * self.bias.unsqueeze(-1)
         ) / (
             batch * neuron_x * neuron_y
         )
 
+        bias_update = 1 - has_spikes + beta_decay * has_spikes
+
         with torch.no_grad():
-            self.weight.add_(update).clip_(0, 1)
+            self.bias.mul_(bias_update)
+            self.weight.add_(weight_update).clip_(0, 1)
 
 
 class RecurColumn(nn.Module):
@@ -494,7 +519,7 @@ class FullDualColumn(nn.Module):
         # iterate time axis: get the winner for each batch, channel of neurons, update winners and depression
         for t in range(time):
             # apply depression state to the potential
-            depress_t = (depression.unsqueeze(-1) == 0).int()
+            depress_t = (depression == 0).unsqueeze(-1).int()
             k_depress_t = ((depression != 0).sum(-1) <
                            self.winners).int().reshape(-1, 1, 1)
             potential_t = (potentials[t] * depress_t *
@@ -535,31 +560,10 @@ class FullDualColumn(nn.Module):
         weight_update = (
             capture_grad * mu_capture * (1 - torch.tanh(self.weight)) +
             backoff_grad * mu_backoff +
-            # * (1 - self.weight) ** 2
             search_grad * mu_search * self.bias.unsqueeze(-1)
         ) / (
             batch * neurons
         )
-
-        # search add bias
-
-        # weight_update = (
-        #     capture_grad * mu_capture * (1 - self.weight) +
-        #     backoff_grad * mu_backoff * self.weight +
-        #     search_grad * mu_search * (1 - self.weight) ** 2
-        # ) / (
-        #     batch * neurons
-        # )
-
-        # weight_update = (
-        #     capture_grad * mu_capture +
-        #     backoff_grad * mu_backoff +
-        #     search_grad * mu_search
-        # ) * (
-        #     (self.weight * (1 - self.weight) * 3 + 0.25)
-        # ) / (
-        #     batch * neurons
-        # )
 
         bias_update = 1 - has_spikes + beta_decay * has_spikes
 
